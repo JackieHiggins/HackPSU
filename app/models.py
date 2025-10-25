df02b6f37ec7c52191b1b53dd071a9246b78cb44
benjlucot <144644460+benjlucot@users.noreply.github.com>
2025-10-25 14:46:27 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,4 +8,20 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256))
 
+class Messages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(100), unique=True, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    created = db.Column(db.timestamp, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+    comments = db.Column(db.Integer, nullable=False)
+
+class Comments(db.Model):
+    id = db.Column(db.Interger, primary_key=True)
+    message_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    created = db.Column(db.timestamp, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
 
