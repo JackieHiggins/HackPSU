1f1b0aaf451121653f098fa718f23fa033364a99
= <=>
2025-10-25 17:16:43 -0400

    hi

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,7 @@
 from flask import Flask
 from config import Config
-<<<<<<< HEAD
 from .extensions import db, login_manager
 from .models import User
-=======
-from .extensions import db
-from flask_login import LoginManager
->>>>>>> ef812f3e8c8748c3e1e620ed5bff9fbbaf5db251
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -32,22 +27,6 @@ def create_app(config_class=Config):
 
     # --- CREATE DATABASE TABLES ---
     with app.app_context():
-<<<<<<< HEAD
         db.create_all()
-=======
-        from . import models # Import models to ensure they are registered with SQLAlchemy
-        db.create_all() # Create the database tables based on the models defined in models.py
-
-    #Configure Flask-Login
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
-
-    # User loader function for Flask-Login
-    from .models import User
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
->>>>>>> ef812f3e8c8748c3e1e620ed5bff9fbbaf5db251
 
     return app
