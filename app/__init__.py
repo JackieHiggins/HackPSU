2906f4c5ee9548e7b2867f8ada2411fa2ee7b745
= <=>
2025-10-25 14:34:59 -0400

    more database init

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import Config
+from .extensions import db
 
 def create_app(config_class=Config):
     # flask instance
@@ -9,7 +10,14 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     # register view blueprint 
-    from .main_views import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    from .main_views import main as main_blueprint # Import the main blueprint from main_views.py
+    app.register_blueprint(main_blueprint) # Register the main blueprint with the Flask app
 
+
+    # initialize database
+    db.init_app(app)
+    # Create the database tables if they don't exist
+    with app.app_context():
+        from . import models # Import models to ensure they are registered with SQLAlchemy
+        db.create_all() # Create the database tables based on the models defined in models.py
     return app
