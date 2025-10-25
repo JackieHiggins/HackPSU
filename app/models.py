e430f3aee2375cb484b705f26328395a69c5ae95
benjlucot <144644460+benjlucot@users.noreply.github.com>
2025-10-25 14:49:05 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,13 +3,13 @@ from datetime import datetime
 
 # The User model represents the 'user' table in the database.
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, unique=True, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256))
 
 class Messages(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, unique=True, primary_key=True)
     user_id = db.Column(db.String(100), unique=True, nullable=False)
     content = db.Column(db.String, nullable=False)
     created = db.Column(db.timestamp, nullable=False)
@@ -17,8 +17,8 @@ class Messages(db.Model):
     comments = db.Column(db.Integer, nullable=False)
 
 class Comments(db.Model):
-    id = db.Column(db.Interger, primary_key=True)
-    message_id = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Interger, unique=True, primary_key=True)
+    message_id = db.Column(db.Integer, unique=True, nullable=False)
     user_id = db.Column(db.Integer, unique=True, nullable=False)
     content = db.Column(db.String, nullable=False)
     created = db.Column(db.timestamp, nullable=False)
