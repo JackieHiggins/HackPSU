de2a7a1069849946314ed1bd779e0acbc7fa6c3c
= <=>
2025-10-25 21:09:22 -0400

    streak stuff

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .extensions import db
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -8,6 +8,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256))
+    current_streak = db.Column(db.Integer, default=0)  # Current posting streak
+    last_story_date = db.Column(db.Date)  # Last date when user posted a story
     
     stories = db.relationship('Story', backref='author', lazy='dynamic')
     comments = db.relationship('Comments', backref='author', lazy='dynamic')
@@ -17,6 +19,26 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def update_streak(self, story_date=None):
+        """Update user's streak based on story post date"""
+        if story_date is None:
+            story_date = date.today()
+            
+        if not self.last_story_date:
+            # First story ever
+            self.current_streak = 1
+        elif story_date == self.last_story_date:
+            # Already posted today, don't update streak
+            return
+        elif story_date == self.last_story_date + timedelta(days=1):
+            # Posted on consecutive day, increment streak
+            self.current_streak += 1
+        else:
+            # Missed a day, reset streak
+            self.current_streak = 1
+            
+        self.last_story_date = story_date
 
 class DailyEmoji(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,6 +56,13 @@ class Story(db.Model):
     daily_emoji_id = db.Column(db.Integer, db.ForeignKey('daily_emoji.id'))
     
     comments = db.relationship('Comments', backref='story', lazy='dynamic')
+    
+    def __init__(self, *args, **kwargs):
+        super(Story, self).__init__(*args, **kwargs)
+        # Update the user's streak when story is created
+        if self.author:
+            self.author.update_streak(self.timestamp.date())
+            db.session.add(self.author)
 
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
